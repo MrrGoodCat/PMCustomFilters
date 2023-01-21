38dd563b34b120b0f0de9b9de2c24598eedf4d5c
mrrgoodcat <mrrgoodcat@gmail.com>
2023-01-21 22:10:13 +0200

    Searchable dropdown for Policy Manager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CustomFilterObjectComponent } from './custom-filter-object/custom-filter-object.component';
@@ -10,7 +11,8 @@ import { CustomFilterObjectComponent } from './custom-filter-object/custom-filte
     CustomFilterObjectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/custom-filter-object/custom-filter-object.component.ts
+++ src/app/custom-filter-object/custom-filter-object.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-custom-filter-object',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./custom-filter-object.component.scss']
 })
 export class CustomFilterObjectComponent implements OnInit {
+  @Input() options: any[];
+  @Input() placeholder: string;
+  @Output() optionSelected = new EventEmitter<any>();
+  public searchQuery = '';
+  public filteredOptions: any[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    this.options = [];
+    this.placeholder = '';
   }
 
+  ngOnInit() {
+    this.options = Array.from({ length: 10 }, (_, i) => `Option ${i + 1}`);
+    this.filteredOptions = this.options;
+  }
+
+  filterOptions() {
+    this.filteredOptions = this.options.filter(o => o.toLowerCase().includes(this.searchQuery.toLowerCase()));
+  }
+
+  onOptionSelected(selectedOption: any) {
+    this.optionSelected.emit(selectedOption);
+  }
 }
